7bf609feca6020fdaf463b5f3c2fc0d66bb7d360
quanghung97 <hungcan1997@gmail.com>
2019-11-08 14:09:34 +0700

    update template

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,24 +6,8 @@
         Air Visual
       </h1>
       <h2 class="subtitle">
-        My Air Visual project
+        Sun* Air Visual project
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
